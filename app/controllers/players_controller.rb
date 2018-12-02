c93edbdd9b45bd49a0c6f3b77f6eb308f68084cc
Sikorskiy <vasiliy.sikorskiy@gmail.com>
2018-12-02 18:33:22 +0300

    added horizontal menu

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,14 +1,20 @@
 class PlayersController < ApplicationController
-  def new_game
+  before_action :set_champions
+
+  def game
     @players_waiting = %w(Wilhelm\ Steinitz Emanuel\ Lasker José\ Raúl\ Capablanca Alexander\ Alekhine)
   end
 
 
-  def about_player
+  def champions
 
   end
 
   def home
     @time = Time.now
+  end
+
+  def set_champions
+    @champions ||= %w(Алехин Стейниц Ботвинник Карпов)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/game', to: 'players#new_game'
-  get '/about', to: 'players#about_player'
+  get '/game', to: 'players#game'
+  get '/champions', to: 'players#champions'
   root 'players#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
